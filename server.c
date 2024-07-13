b5004c77f350f585fb0bc19ae88175d82cc4fbff
Avyukt Soni <soniavyukt@gmail.com>
2024-07-13 12:22:56 +0530

    server complete

--- server.c
+++ server.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    int sockfd, newsockdf, portno, n;
+    int sockfd, newsockfd, portno, n;
     char buffer[255];
 
     struct sockaddr_in serv_addr, cli_addr;
@@ -39,5 +39,43 @@ int main(int argc, char *argv[]){
     if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
         error("Binding Failed");
     }
+
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+    if(newsockfd < 0){
+        error("Error on Accept");
+    }
+
+    while(1){
+        bzero(buffer, 255);
+        n = read(newsockfd, buffer, 255);
+        
+        if(n < 0){
+            error("Error on reading");
+        }
+
+        printf("Client: %s\n", buffer);
+        bzero(buffer, 255);
+        fgets(buffer, 255, stdin);
+
+        n = write(newsockfd, buffer, strlen(buffer));
+
+        if(n < 0){
+            error("Error on Writing");
+        }
+        int i = strncmp("Bye", buffer, 3);
+
+        if(i == 0){
+            break;
+        }
+    }
+
+    close(newsockfd);
+    close(sockfd);
+
+    return 0;
 
 }
